59903d5c693bd2ffc155cdac67a614b51a271587
Ancy Lazar <ancylazar@gmail.com>
2026-08-05 22:27:24 +0530

    Add max shelf function

--- library.py
+++ library.py
@@ -3,3 +3,10 @@ def total_books(shelves):
 if __name__ == "__main__": 
     shelves = [12, 8, 15] 
     print(total_books(shelves))
+def total_books(shelves): 
+    return sum(shelves) 
+def max_shelf(shelves): 
+    return max(shelves) 
+if __name__ == "__main__": 
+    shelves=[12,8,15] 
+    print(total_books(shelves))
